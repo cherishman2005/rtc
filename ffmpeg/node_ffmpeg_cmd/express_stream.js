496a3b60a424f7e81e74d292af1858fadfb5982b
zhangbiwu <cherishman2005@163.com>
2022-01-23 21:39:30 +0800

    Add files via upload

--- ffmpeg/node_ffmpeg_cmd/express_stream.js
+++ ffmpeg/node_ffmpeg_cmd/express_stream.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = '/usr/bin/ffmpeg';
-
 const app = express();
 
-app.get('/video/:filename', function(req, res) {
+const videoStream = async function (req, res) {
   res.contentType('flv');
   // make sure you set the correct path to your video file storage
   const pathToMovie = '/data/services/nodejs/ffmpeg_cmd/' + req.params.filename;
@@ -21,6 +20,38 @@ app.get('/video/:filename', function(req, res) {
     })
     // save to stream
     .pipe(res, {end:true});
-});
+}
+
+const snapshot = async function (req, res) {
+    res.contentType('image/png');
+    const inputPath = 'https://yy-ai-train.bj.bcebos.com/dataset/zhangbiwu/video/test.mp4';
+    let file = __dirname + '/snapshot-01.png';
+
+    let proc = ffmpeg(inputPath)
+        .setFfmpegPath(ffmpegPath)
+        .on('end', function(files)
+        {
+            //res.sendfile(file);
+            console.log('end');
+        })
+        .on('error', function(err)
+        {
+            /*
+            res.json({
+                status : 'error',
+                error : err.message
+            });
+            */
+            console.log(err);
+        })
+        .outputOptions(['-f image2', '-vframes 5', '-vcodec png', '-f rawvideo', '-s 320x240', '-ss 00:02:01'])
+        //.output(file)
+        //.run();
+        // save to stream
+        .pipe(res, {end:true});
+}
+
+app.get('/video/:filename', videoStream);
+app.get('/images/:filename', snapshot);
 
 app.listen(4000);
