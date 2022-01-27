855f99e27f8ed004f0b4d23cc15acc775d5fbd5b
zhangbiwu <cherishman2005@163.com>
2022-01-27 17:48:35 +0800

    client-app

--- ffmpeg/node_ffmpeg_cmd/stream_example/app.js
+++ ffmpeg/node_ffmpeg_cmd/stream_example/app.js
@@ -6,39 +6,14 @@ const axios = require('axios');
 const client = require('./bce_client');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = '/usr/bin/ffmpeg';
-//const app = express();
+const app = express();
 const http = require('http');
-const Koa = require('koa');
-const app = new Koa();
+//const Koa = require('koa');
+//const app = new Koa();
 
 const router = express.Router();
+
 /*
-app.use(router.post("/uploadFile",(ctx,next)=>{
-    //let data = fs.createReadStream(ctx)
-       //ctx.req.setEncoding="binary";
-       let url=path.join(__dirname,"/test/");
-       let file = path.join(url,"node.tar.gz")
-       let chunks='';
-       
-         ctx.req.on("data",function(d){
-            let da = d.toString("utf8")
-            debugger
-         }).on("close",function(e){
-            debugger
-         }).on("error",function(){
-             debugger
-         }).on("end",function(){
-             debugger
-         })
-        
-        ctx.body = 'Hello World';
-       next();
-}))
-
-http.createServer(app.callback()).listen(3000);
-*/
-
-
 let server = http.createServer(function(req, res) {
   let writer = fs.createWriteStream('test_stream.jpg') 
 
@@ -54,3 +29,26 @@ let server = http.createServer(function(req, res) {
 });
 
 server.listen(3000);
+*/
+
+
+
+app.use(router.post('/upload/:filename', (req, res, next) => {
+  console.log("params.filename:", req.params.filename);
+  let filename = req.params.filename;
+  console.log("headers", req.headers);
+  let buffers = [];
+  req.on('data',(trunk) => {
+    console.info(trunk.length);
+    buffers.push(trunk);
+  }).on('end',async () => {
+      const buffer= Buffer.concat(buffers);
+      fs.writeFileSync(filename, buffer);
+      return res.end();
+    }).on('close', () => {
+      res.status(400).json({"err_code":"40000"});
+    }).on('error', () => {
+      res.status(400).json({"err_code":"40000"});
+    });
+}))
+app.listen(3000);
